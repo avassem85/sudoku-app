c7aa65042a4e8d2f9192287f2165487c1902780d
Alex van Assem <avassem@gmail.com>
2016-09-30 20:33:16 +0200

    Add BrowserSync in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,43 @@ var gutil = require('gulp-load-utils')(['colors', 'env', 'log', 'pipeline']);
 var config = require('./gulp.config')();
 var $ = require('gulp-load-plugins')({lazy: true});
 
+var historyApiFallback = require('connect-history-api-fallback');
+var browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
 require('web-component-tester').gulp.init(gulp);
+
+// Watch files for changes & reload
+gulp.task('serve', ['jshint'], function() {
+  browserSync({
+    port: 5000,
+    notify: false,
+    logPrefix: 'PSK',
+    snippetOptions: {
+      rule: {
+        match: '<span id="browser-sync-binding"></span>',
+        fn: function(snippet) {
+          return snippet;
+        }
+      }
+    },
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.'],
+      middleware: [historyApiFallback()],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch(['src/**/*.html'], ['jshint', reload]);
+  gulp.watch(['src/{scripts,elements}/**/{*.js,*.html}'], ['jshint']);
+  gulp.watch(['src/images/**/*'], reload);
+});
 
 gulp.task('default', ['test:local'], function() {
   gulp.watch(['elements/**', 'test/**'], ['test:local']);
