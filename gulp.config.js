0ad4d470f1222252f954479a38a209a3e05cfe05
Alex van Assem <avassem@gmail.com>
2016-09-30 19:36:13 +0200

    Add Browser-Sync

--- gulp.config.js
+++ gulp.config.js
@@ -3,10 +3,8 @@ module.exports = function() {
     alljs: [
       './src/**/*.js',
       './*.js',
-    ],
-    allhtml: [
       './src/**/*.html',
-    ],
+    ]
   };
 
   return config;
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var args = require('yargs').argv;
 var gutil = require('gulp-load-utils')(['colors', 'env', 'log', 'pipeline']);
 var config = require('./gulp.config')();
-
 var $ = require('gulp-load-plugins')({lazy: true});
 
 require('web-component-tester').gulp.init(gulp);
@@ -11,25 +10,13 @@ gulp.task('default', ['test:local'], function() {
   gulp.watch(['elements/**', 'test/**'], ['test:local']);
 });
 
-gulp.task('jshint:js', function() {
-  log('Analyzing JS source with JSHint and JSCS');
+gulp.task('jshint', function() {
+  log('Analyzing source with JSHint and JSCS');
   return gulp
     .src(config.alljs)
     .pipe($.if(args.verbose, $.print()))
     .pipe($.jscs())
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
-    .pipe($.jshint.reporter('fail'));
-});
-
-gulp.task('jshint:html', function() {
-  log('Analyzing HTML source with JSHint and JSCS');
-  return gulp
-    .src(config.allhtml)
-    .pipe($.htmlExtract({
-      sel: 'script, code.javascript'
-    }))
-    .pipe($.jscs())
+    .pipe($.jshint.extract())
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
     .pipe($.jshint.reporter('fail'));
